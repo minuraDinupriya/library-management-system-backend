accef95e773e8401b3dcf8df7e289057aca9a571
Minura Dinupriya <mranaweera793@gmail.com>
2024-04-08 05:53:53 +0530

    remove unnecessary comments

--- src/main/java/org/example/entity/BookEntity.java
+++ src/main/java/org/example/entity/BookEntity.java
@@ -12,7 +12,6 @@ public class BookEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-//    @OneToOne(mappedBy = "isbn")
     private Long isbn;
     private String title;
     private String author;
